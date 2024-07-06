4fc52775bd4a82a942b96a4870d4e97e99e8bd5e
illiarerun <ilya.vovchenko@gmail.com>
2024-07-07 00:51:50 +0300

    add local env support for db settings

--- config/dev.exs
+++ config/dev.exs
@@ -2,13 +2,13 @@ import Config
 
 # Configure your database
 config :elixir_posts, ElixirPosts.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "elixir_posts_dev",
+  username: System.get_env("PG_USER", "postgres"),
+  password: System.get_env("PG_PASSWORD", "postgres"),
+  hostname: System.get_env("PG_HOSTNAME", "localhost"),
+  database: System.get_env("PG_DBNAME", "postgres"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: String.to_integer(System.get_env("DB_POOL_SIZE", "10"))
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule ElixirPosts.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["phx.digest"]
+      "assets.deploy": ["phx.digest"],
+      "run.env": ["cmd source .env.dev && mix phx.server"],
     ]
   end
 end
